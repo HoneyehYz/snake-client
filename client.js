7a2b9907da91481229de26580001c3cbcbb31d6f
HoneyehYz <yazdizadehhoneyeh@yahoo.com>
2020-09-23 00:36:35 +0000

    Move Hardcoded

--- client.js
+++ client.js
@@ -12,10 +12,13 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   conn.on('connect', () => {console.log('Successfully connected to game server');});
+  conn.on('connect', () => {conn.write("Move: up")});
   conn.on('data', (data) => {
   console.log('Server says: ', data);
-  console.log('Name: ', "HYZ")
+  console.log('Name: ', "HYZ");
+  
 });
+  
   
   return conn;
 }
