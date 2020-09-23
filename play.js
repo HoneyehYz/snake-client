ab3e9d89bfa5b96720762584c991c7e08c4d0e4d
HoneyehYz <yazdizadehhoneyeh@yahoo.com>
2020-09-23 00:52:32 +0000

    ctrl + c handler

--- play.js
+++ play.js
@@ -1,7 +1,43 @@
 //import { connect } from './client.js';
 const { connect } = require('./client');
 const net = require('net');
+const { stdin } = require('process');
 
 console.log('Connecting ...');
 connect();
 
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+
+const handleUserInput = function(){
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  return stdin;
+
+}
+
+
+setupInput(handleUserInput());
+
+
+
+
+// stdin.on('data', (key) => {
+  // if (key === '\u0003') {
+  //   process.exit();
+  // }
+//   process.stdout.write('.');
+// });
